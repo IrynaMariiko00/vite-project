18e62cd7ea1d4e5184d94b1fdca498a1f09bc48e
Iryna Mariiko <IrynaMM497@gmail.com>
2025-04-26 10:21:42 +0300

    First commit

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      prettier,
+      react,
+      "react-hooks": reactHooks,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.scss";
+
+function App() {
+  return <button className="btn">K</button>;
+}
+
+export default App;
